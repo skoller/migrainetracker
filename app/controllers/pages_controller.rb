282b0a87824893bcf83170c1b405300283417763
skoller <nskoller@gmail.com>
2010-04-21 14:27:13 -0700

    help page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,13 +1,21 @@
 class PagesController < ApplicationController
   def home
     @title = 'Home'
+    @base_title = 'HeadacheTRACKER'
   end
   
   def contact
     @title = 'Contact'
+    @base_title = 'HeadacheTRACKER'
   end
   
   def about
     @title = 'About'
+    @base_title = 'HeadacheTRACKER'
+  end
+  
+  def help
+    @title = 'Help'
+    @base_title = 'HeadacheTRACKER'
   end
 end
