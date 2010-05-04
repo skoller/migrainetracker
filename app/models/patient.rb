2df3562d0bea432fb7b251b1a7a305561b9e7e73
skoller <nskoller@gmail.com>
2010-05-03 23:30:37 -0700

    fixed

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,12 +1,12 @@
 require 'digest'
 class Patient < ActiveRecord::Base
   attr_accessor :password
-  attr_accessible (:name, :email, :password, :password_confirmation)
+  attr_accessible(:name, :email, :password, :password_confirmation)
   EmailRegex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates_confirmation_of :password
   validates_presence_of :password
   #validates_presence_of :password, :within 6..40
-  validates_presence_of (:name, :email)
+  validates_presence_of(:name, :email)
   validates_length_of :name, :maximum => 50
   validates_format_of :email, :with => EmailRegex
   validates_uniqueness_of :email, :case_sensitive => false
@@ -19,9 +19,9 @@ class Patient < ActiveRecord::Base
     save_without_validation
   end
   def self.authenticate(email,submitted_password)
-    user = find_by_email(email)
-    return nil  if user.nil?
-    return user if user.has_password?(submitted_password)
+    patient = find_by_email(email)
+    return nil  if patient.nil?
+    return patient if patient.has_password?(submitted_password)
   end
   has_many :headaches
   private
